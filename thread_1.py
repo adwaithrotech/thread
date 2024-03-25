202b0d7a67548b1b34edab13f33283204a5ab448
adwaithrotech <adwaithrotechai@gmail.com>
2024-03-25 12:36:47 +0530

    added program without thread

---
+++ thread_1.py
@@ -0,0 +1,15 @@
+import time
+
+start=time.perf_counter()
+
+def do_something():
+    print("sleeping 1 second")
+    time.sleep(1)
+    print("done sleeping")
+for i in range(10):
+    do_something()
+
+
+finish=time.perf_counter()
+
+print(f'Finished in :{round(finish-start,2)} seconds')
