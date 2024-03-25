a6c70722b99635ee3ff19c2705115f3c32c821dc
adwaithrotech <adwaithrotechai@gmail.com>
2024-03-25 13:10:43 +0530

    added sample thread

--- thread_1.py
+++ thread_1.py
@@ -1,17 +1,18 @@
 import time
-
+import threading
 start=time.perf_counter()
 
 def do_something():
     print("sleeping 1 second")
     time.sleep(1)
     print("done sleeping")
-for i in range(10):
-    do_something()
 
-
+t1=threading.Thread(target=do_something)
+t2=threading.Thread(target=do_something)
+t1.start()
+t2.start()
+t1.join()
+t2.join()
 finish=time.perf_counter()
-
 print(f'Finished in :{round(finish-start,2)} seconds') 
 
-#sample
